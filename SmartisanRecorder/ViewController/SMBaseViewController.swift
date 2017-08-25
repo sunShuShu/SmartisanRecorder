5baff4e888a2553ab0690610f8c2bb7a8924deb7
sunShuShu <13247774+sunShuShu@users.noreply.github.com>
2017-08-25 19:33:04 +0800

    Add base view controller, record user stay time in every view by UMeng;

---
+++ SmartisanRecorder/ViewController/SMBaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SMBaseViewController.swift
+//  SmartisanRecorder
+//
+//  Created by sunda on 25/08/2017.
+//  Copyright © 2017 sunShuShu. All rights reserved.
+//
+
+import Foundation
+
+class SMBaseViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        MobClick.beginLogPageView("\(type(of: self))");
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+         MobClick.endLogPageView("\(type(of: self))");
+    }
+}
