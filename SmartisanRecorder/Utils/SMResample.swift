c513fec616201e04adff385075c4ee84f3e2bab3
sunShuShu <13247774+sunShuShu@users.noreply.github.com>
2017-10-10 17:50:45 +0800

    Merge interpolate and degrade to single method, return result buffer and length;

--- SmartisanRecorder/Utils/SMResample.swift
+++ SmartisanRecorder/Utils/SMResample.swift
@@ -10,8 +10,24 @@ import Foundation
 
 class SMResample {
     
+    /// Resample audio data in buffer
+    ///
+    /// - Parameters:
+    ///   - times: resample times. positive number for interpolate, negative for degrade
+    ///   - buffer: audio data buffer
+    ///   - length: buffer length
+    /// - Returns: (resampled buffer, resampled buffer length)
+    static func resample(_ times: Int,  buffer: UnsafeMutablePointer<UInt8>, length: Int) -> (buffer:UnsafeMutablePointer<UInt8>, length:Int) {
+        if times < 0 {
+            return (degrade(-times, buffer: buffer, length: length), length / -times)
+        } else {
+            
+            return (interpolate(times, buffer: buffer, length: length), times * length)
+        }
+    }
+    
     //Currently only 16-bit PCM data is supported
-    static func interpolate(_ times: Int, buffer: UnsafeMutablePointer<UInt8>, length: Int) -> UnsafeMutablePointer<UInt8> {
+    private static func interpolate(_ times: Int, buffer: UnsafeMutablePointer<UInt8>, length: Int) -> UnsafeMutablePointer<UInt8> {
         let output = UnsafeMutablePointer<UInt8>.allocate(capacity: length * times)
         for i in stride(from: 0, to: length - 1, by: 2) {
             let high8Bits = buffer.advanced(by: i).pointee
@@ -25,8 +41,8 @@ class SMResample {
     }
     
     //Currently only 16-bit PCM data is supported
-    static func degrade(_ times: Int, buffer: UnsafeMutablePointer<UInt8>, length: Int) -> UnsafeMutablePointer<UInt8> {
-        let output = UnsafeMutablePointer<UInt8>.allocate(capacity: length * times)
+    private static func degrade(_ times: Int, buffer: UnsafeMutablePointer<UInt8>, length: Int) -> UnsafeMutablePointer<UInt8> {
+        let output = UnsafeMutablePointer<UInt8>.allocate(capacity: length / times)
         for i in stride(from: 0, to: length - 1, by: 2 * times) {
             let high8Bits = buffer.advanced(by: i).pointee
             let low8Bits = buffer.advanced(by: i+1).pointee
