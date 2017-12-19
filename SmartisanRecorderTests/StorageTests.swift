525a691687695d89fb544e96d7888324bd1051d3
sunShuShu <13247774+sunShuShu@users.noreply.github.com>
2017-12-19 16:34:48 +0800

    Add unit test code of database storage;

--- SmartisanRecorderTests/StorageTests.swift
+++ SmartisanRecorderTests/StorageTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class SMStorageTest: XCTestCase {
     
+    let optionTimes = 5
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,25 +23,59 @@ class SMStorageTest: XCTestCase {
         super.tearDown()
     }
     
-    func testTable() {
-        let url = URL.init(string: NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first! + "/test.db")
-        print(url?.absoluteString as Any)
-        var table = "table_\(arc4random() % 999)";
-        table = "table_552"
-        let storage = SMStorage.init(databasePath: url, errorBlock: nil)
+    func testDatabase() {
+        objc_sync_enter(self)
+        let filePath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first! + "/test.db"
+        let url = URL.init(fileURLWithPath: filePath)
+        print(url.absoluteString as Any)
+        var storage = SMStorage.init(databasePath: filePath,
+                                     table: "SMAudioFile",
+                                     class: SMFileStorageModel.self,
+                                     errorBlock: { error in
+                                        XCTAssert(false, "\(error)")
+        })
         XCTAssertNotNil(storage)
-        let createResult = storage!.createTable(table, class: SMFileStorageModel.self)
-        XCTAssert(createResult)
+
+        var allModel = [SMFileStorageModel]()
+        for _ in 0..<optionTimes {
+            let model = SMFileStorageModel();
+            model.name = "Rec_录音_\(arc4random() % 9999)"
+            model.voiceType = SMVoiceTypePhontCall
+            model.md5 = "fhdjsklafh436543dsjkalfjdsafdsa5555fdhsg5555"
+            model.pointCount = 5
+            model.pointFileName = "I am point file name"
+            model.waveformFileName = "Waveform File Name"
+            let insertResult = storage?.insert(model)
+            XCTAssert(model.localID != 0)
+            XCTAssertNotNil(insertResult)
+            allModel.append(model)
+        }
+        
+        var allObjects = storage?.getAllObjects()
+        XCTAssert(allObjects?.count == optionTimes)
+        
         let model = SMFileStorageModel();
-        model.name = "rec_name_234"
+        model.name = "Rec_录音_test_update"
         model.voiceType = SMVoiceTypePhontCall
         model.md5 = "fhdjsklafh436543dsjkalfjdsafdsa5555fdhsg5555"
         model.pointCount = 5
         model.pointFileName = "I am point file name"
         model.waveformFileName = "Waveform File Name"
-        let insertResult = storage!.insert(model, intoTable: table)
-        XCTAssert(insertResult)
-        //storage?.dropTable(table)
+        model.setValue(1, forKey: "localID")
+        let updateResult = storage?.update(model)
+        XCTAssertNotNil(updateResult)
+        
+        let deleteResult = storage?.deleteObject(1)
+        XCTAssertNotNil(deleteResult)
+        allObjects = storage?.getAllObjects()
+        XCTAssert(allObjects?.count == optionTimes - 1)
+        storage = nil
+        
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch  {
+            XCTAssert(false, error.localizedDescription)
+        }
+        objc_sync_exit(self)
     }
-    
 }
