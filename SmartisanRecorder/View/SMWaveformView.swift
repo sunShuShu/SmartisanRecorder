38ed975b79ae10b2794059ac0e257bba58d8e764
sunShuShu <13247774+sunShuShu@users.noreply.github.com>
2017-12-25 17:48:34 +0800

    temp

--- SmartisanRecorder/View/SMWaveformView.swift
+++ SmartisanRecorder/View/SMWaveformView.swift
@@ -13,14 +13,28 @@ import CoreGraphics
 class SMWaveformView: UIView {
     static var maxPowerLevel = CGFloat(UInt8.max)
     /// line width(point) e.g. 1 point = 2 pixels in iPhone7, 1 point = 3 pixels in plus series
-    var lineWidth: CGFloat = 1
+    var lineWidth: CGFloat = 1 {
+        didSet {
+            guard lineWidth > 0 else {
+                lineWidth = oldValue
+                assert(false)
+            }
+        }
+    }
+    var linesPerSecond: Int = 50
+    var currentOffset: CGFloat = 0
+    var totalWidth: CGFloat = 0
+    
     /// line color
     var color: CGColor = UIColor.black.cgColor
-    /// line height(point)
+    
     private var path = CGMutablePath()
     var powerLevel: [UInt8] = Array() {
         didSet {
             self.path = CGMutablePath()
+            let totleLines: Int = Int(self.bounds.size.width / self.lineWidth) + 1
+            let renderedLevel = (powerLevel.count * totalWidth) / self.bounds.size.width
+            
             for (index, oneLevel) in powerLevel.enumerated() {
                 let height = (CGFloat(oneLevel) * bounds.height) / CGFloat(SMWaveformView.maxPowerLevel)
                 let x = CGFloat(index) * lineWidth
